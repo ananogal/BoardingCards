f9e3354238167a28de9bc2ec881c591a7e002de7
Ana Nogal <ana.nogal@gmail.com>
2015-02-01 18:00:38 +0000

    first commit.

---
+++ BoardingCards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoardingCards
+//
+//  Created by Ana Nogal on 01/02/2015.
+//  Copyright (c) 2015 AnaNogal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
