f291876997c68e33c6e49b853ec3abcae3e245cd
ananogal <ana.nogal@gmail.com>
2015-02-01 21:14:43 +0000

    Added BoardingCardCell

---
+++ BoardingCards/BoardingCardCell.swift
@@ -0,0 +1,17 @@
+//
+//  BoardingCardCell.swift
+//  BoardingCards
+//
+//  Created by Ana Nogal on 01/02/2015.
+//  Copyright (c) 2015 AnaNogal. All rights reserved.
+//
+
+import UIKit
+
+public class BoardingCardCell: UITableViewCell {
+
+    @IBOutlet var lblCardIdentifier: UILabel!
+    @IBOutlet var lblOrigin: UILabel!
+    @IBOutlet var lblDestination: UILabel!
+    @IBOutlet var lblRoute: UILabel!
+}
